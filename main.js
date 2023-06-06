654408dc67bb67dc3edf0d0b590889a4b7aba6e9
Matheus Guimarães <guimaraesmrocha@gmail.com>
2023-06-05 21:00:12 -0300

    Limpando codigo

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ function tocaSom(seletorAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
